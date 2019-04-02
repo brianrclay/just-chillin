047323513b40d570c7f8274ff6c02531fe055a12
Brian Clay <brian@blueroof360.com>
2019-04-02 01:51:01 -0600

    js changes

--- main.js
+++ main.js
@@ -6,19 +6,39 @@ $(document).ready(function(){
         $('.menu').removeClass('open');
     })
 
+    var topPadding = '60px';
+    var topScrollPadding = '30px 60px';
+    var wWidth = $(window).width();
+    if(wWidth <= 768){
+        topPadding = '30px'
+        topScrollPadding = '30px'
+    }
+
     $('.menu').scroll(function(){
         var menuTop = $('.menu').scrollTop();
-        var wWidth = $(window).width();
-        if(menuTop > 30 && wWidth >= 768){
+        if(menuTop > 30){
             $('.menu .menu-top').css({
-                'padding': '30px 60px',
+                'padding': topScrollPadding,
                 'background-color': '#fff',
                 'box-shadow': '0px 2px 6px rgba(0, 0, 0, .2)',
             });
         } else{
             $('.menu .menu-top').css({
                 'box-shadow': 'none',
-                'padding': '60px',
+                'padding': topPadding,
+                'background-color': 'transparent'
+            });
+        }
+        if(menuTop > 30){
+            $('.menu .menu-top').css({
+                'padding': topScrollPadding,
+                'background-color': '#fff',
+                'box-shadow': '0px 2px 6px rgba(0, 0, 0, .2)',
+            });
+        } else{
+            $('.menu .menu-top').css({
+                'box-shadow': 'none',
+                'padding': topPadding,
                 'background-color': 'transparent'
             });
         }
